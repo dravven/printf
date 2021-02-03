ec4908c12f412ea51df117d17350973cad6c23ef
dravven <danild98@gmail.com>
2021-02-03 13:06:42 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Dsons/CLionProjects/untitled/main.c" "C:/Users/Dsons/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/main.c.obj"
+  "C:/Users/Dsons/CLionProjects/untitled/parser.c" "C:/Users/Dsons/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/parser.c.obj"
+  "C:/Users/Dsons/CLionProjects/untitled/parser_width.c" "C:/Users/Dsons/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/parser_width.c.obj"
+  "C:/Users/Dsons/CLionProjects/untitled/precision_parse.c" "C:/Users/Dsons/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/precision_parse.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/main.c.obj"
+  "CMakeFiles/untitled.dir/parser.c.obj"
+  "CMakeFiles/untitled.dir/parser_width.c.obj"
+  "CMakeFiles/untitled.dir/precision_parse.c.obj"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
